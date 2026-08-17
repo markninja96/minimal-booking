13221284953f856c58aaeb3ece89fae8537b133b
njihiamark <njihiamark@gmail.com>
2026-08-17 04:37:55 +0300

    feat: add metrics and e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { BookingsModule } from './bookings/bookings.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
+import { MetricsModule } from './metrics/metrics.module';
 import { WebsocketModule } from './websocket/websocket.module';
 
 @Module({
@@ -13,6 +14,7 @@ import { WebsocketModule } from './websocket/websocket.module';
     AuthModule,
     DatabaseModule,
     HealthModule,
+    MetricsModule,
     BookingsModule,
     WebsocketModule,
   ],
